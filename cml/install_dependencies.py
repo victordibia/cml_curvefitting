17f917a8c163237022cf65acd697edc09313714a
Victor Dibia <victor.dibia@gmail.com>
2021-04-29 10:22:10 -0700

    Update install_dependencies.py

--- cml/install_dependencies.py
+++ cml/install_dependencies.py
@@ -3,5 +3,5 @@
 model_build_commands = """#!/bin/bash
             pip3 install -r requirements.txt"""
 
-with open("cdswbuild.sh", "w") as file:
+with open("cdsw-build.sh", "w") as file:
     file.write(model_build_commands)
