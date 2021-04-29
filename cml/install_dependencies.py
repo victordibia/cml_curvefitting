e00ed98bd5b3e34399b357246c53b81a2e21dbe3
Victor Dibia <victor.dibia@gmail.com>
2021-04-29 13:12:42 -0700

    add cdsw build to install dependencies in model endpoint

--- cml/install_dependencies.py
+++ cml/install_dependencies.py
@@ -1,7 +1,7 @@
 !pip3 install -r requirements.txt 
 
-model_build_commands = """#!/bin/bash
-pip3 install -r requirements.txt"""
+# model_build_commands = """#!/bin/bash
+# pip3 install -r requirements.txt"""
 
-with open("cdsw-build.sh", "w") as file:
-    file.write(model_build_commands)
+# with open("cdsw-build.sh", "w") as file:
+#     file.write(model_build_commands)
